3dac89b67ef59fa31e8bf6569c103d0539db6c15
mgumiero9 <mgumiero9@gmail.com>
2016-10-09 21:58:11 -0300

    firstCommit

---
+++ www/js/index_user_scripts.js
@@ -0,0 +1,29 @@
+/*jshint browser:true */
+/*global $ */(function()
+{
+ "use strict";
+ /*
+   hook up event handlers 
+ */
+ function register_event_handlers()
+ {
+    
+    
+     /* button  #btn_menu */
+    $(document).on("click", "#btn_menu", function(evt)
+    {
+         /*global uib_sb */
+         /* Other possible functions are: 
+           uib_sb.open_sidebar($sb)
+           uib_sb.close_sidebar($sb)
+           uib_sb.toggle_sidebar($sb)
+            uib_sb.close_all_sidebars()
+          See js/sidebar.js for the full sidebar API */
+        
+         uib_sb.toggle_sidebar($(".uib_w_3"));  
+         return false;
+    });
+    
+    }
+ document.addEventListener("app.Ready", register_event_handlers, false);
+})();
