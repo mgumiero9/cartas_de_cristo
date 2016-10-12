ad121bb8120819322704056bf454da72d1555184
mgumiero9 <mgumiero9@gmail.com>
2016-10-11 21:35:02 -0300

    adjusting left_sidebar

--- www/js/index_user_scripts.js
+++ www/js/index_user_scripts.js
@@ -16,20 +16,10 @@
     
     
         /* listitem  #btn_leitura */
-    $(document).on("click", "#btn_leitura", function(evt)
-    {
-         /*global activate_page */
-         activate_page("#uib_page_reading2"); 
-         return false;
-    });
+    
     
         /* listitem  Cartas de Cristo2 */
-    $(document).on("click", ".uib_w_6", function(evt)
-    {
-         /*global activate_subpage */
-         activate_subpage("#uib_page_reading"); 
-         return false;
-    });
+    
     
         /* button  #btn_back */
     $(document).on("click", "#btn_back", function(evt)
@@ -40,7 +30,33 @@
     });
 
         /* button  #btn_menu */
+    
+    
+        /* button  #btn_menu */
     $(document).on("click", "#btn_menu", function(evt)
+    {
+         /*global uib_sb */
+         /* Other possible functions are: 
+           uib_sb.open_sidebar($sb)
+           uib_sb.close_sidebar($sb)
+           uib_sb.toggle_sidebar($sb)
+            uib_sb.close_all_sidebars()
+          See js/sidebar.js for the full sidebar API */
+        
+         uib_sb.toggle_sidebar($("#sidebar_left"));  
+         return false;
+    });
+    
+        /* listitem  #btn_leitura */
+    $(document).on("click", "#btn_leitura", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#uib_page_reading"); 
+         return false;
+    });
+    
+        /* listitem  Cartas de Cristo2 */
+    $(document).on("click", ".uib_w_6", function(evt)
     {
          /*global activate_page */
          activate_page("#uib_page_reading"); 
