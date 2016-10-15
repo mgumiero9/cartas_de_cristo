a64122637373ed19e2f8f9ee17ad14e82157838b
mgumiero9 <mgumiero9@gmail.com>
2016-10-15 19:43:14 -0300

    adjusting sidebar_left

--- www/js/index_user_scripts.js
+++ www/js/index_user_scripts.js
@@ -8,31 +8,14 @@
  function register_event_handlers()
  {
     
-    
-     /* button  #btn_menu */
-    
-    
-        /* listitem  #btn_leitura */
-    
-    
-        /* listitem  #btn_leitura */
-    
-    
-        /* listitem  Cartas de Cristo2 */
-    
-    
-        /* button  #btn_back */
     $(document).on("click", "#btn_back", function(evt)
     {
          /*global activate_page */
          activate_page("#mainpage");
+         uib_sb.close_sidebar($("#sidebar_left")); 
          return false;
     });
 
-        /* button  #btn_menu */
-    
-    
-        /* button  #btn_menu */
     $(document).on("click", "#btn_menu", function(evt)
     {
          /*global uib_sb */
